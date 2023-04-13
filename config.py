f9e56ce0b6af129df47f7b45928ba77db1dd83f9
Elis Sammi <62981106+elissammi@users.noreply.github.com>
2023-04-13 12:44:38 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.c4gehvhkqswz.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "sammi-employee"
+customregion = "us-east-1"
